07a93523d2a0c81006ce1678b9589695585b40a1
seungm2n <110989761+seungm2n@users.noreply.github.com>
2023-12-01 21:50:30 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,16 +2,12 @@
 
 ROOT_PATH="/home/ubuntu/app"
 JAR="$ROOT_PATH/application.jar"
-
 STOP_LOG="$ROOT_PATH/log/stop.log"
-
-SERVICE_PID=$(pgrep -f $JAR)
-
-NOW=$(date +%c)
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "[$NOW] 서비스 NouFound" >> $STOP_LOG
+  echo "서비스 NouFound" >> $STOP_LOG
 else
-  echo "[$NOW] 서비스 종료 " >> $STOP_LOG
+  echo "서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
 fi
